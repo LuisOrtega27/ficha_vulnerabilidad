ad864ade571c527971408d2c05e5bc699329d3a0
Fernan <lfortega0625@gmail.com>
2026-08-05 22:34:17 -0400

    wip: migration to react and tailwind  start

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+
+  return (
+    <div className="bg-light-main min-h-lvh min-w-lvw flex items-center justify-center">
+      
+      <div className="bg-white w-8/12 p-4 rounded-2xl shadow-md">
+
+        <h1 className="text-4xl text-green-500">Hello world</h1>
+
+      </div>
+
+
+      
+    </div>
+  )
+}
+
+export default App
